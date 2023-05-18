20b2c5805ba64d9839f67e966068115758a8816d
Nicolas Aquino <aquino_nicolas@hotmail.com>
2023-05-18 14:55:48 -0300

    Inicia ej1

---
+++ ej1/ej1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+    int n, m;       //n = cant. vertices
+    cin >> n >> m;  //m = cant. aristas
+
+    //TODO: Añadir vertices (lista de adyacencias?)
+    while (m--){
+        //TODO: Añadir las aristas
+    }
+
+    //TODO: Resolver el ejercicio T_T
+
+    return 0;
+}
