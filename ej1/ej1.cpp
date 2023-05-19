51f718da5908185e1e05eb6ccaea235b4f86f28f
Nicolas Aquino <aquino_nicolas@hotmail.com>
2023-05-19 11:52:52 -0300

    Fix adj list bug

--- ej1/ej1.cpp
+++ ej1/ej1.cpp
@@ -11,8 +11,8 @@ int main(int argc, char *argv[]){
     cin >> n >> m;  //m = cant. aristas
 
     //Añadir vertices (Init adjacency list)
-    adj = vector<list<uint>>(n);
-    for (uint i = 0; i < n; i++)
+    adj = vector<list<uint>>(n+1); //El vertice 0 no lo usamos
+    for (uint i = 1; i <= n; i++)
         adj[i] = list<uint>(); //Inicializa lista vacía
     
     while (m--) { //Agregamos las aristas
@@ -21,6 +21,18 @@ int main(int argc, char *argv[]){
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
+
+    //Debug
+    /*
+    cout << 0 << ": UNUSED" << endl;
+    for (uint i = 1; i < adj.size(); i++){
+        cout << i << ": { ";
+        list<uint>::iterator it;
+        for(it = adj[i].begin(); it != adj[i].end(); it++){
+            cout << *it << " ";
+        }
+        cout << "}" << endl;
+    }*/
 
     //TODO: Resolver el ejercicio T_T
 
