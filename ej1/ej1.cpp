538458af77fd5e35f9c31207294c7cdc7561d7a8
Nicolas Aquino <aquino_nicolas@hotmail.com>
2023-05-19 11:41:15 -0300

    Init adjacency list

--- ej1/ej1.cpp
+++ ej1/ej1.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
+#include <vector>
+#include <list>
 
 using namespace std;
+
+vector<list<uint>> adj; //Lista de adyacencias
 
 int main(int argc, char *argv[]){
     int n, m;       //n = cant. vertices
     cin >> n >> m;  //m = cant. aristas
 
-    //TODO: Añadir vertices (lista de adyacencias?)
-    while (m--){
-        //TODO: Añadir las aristas
+    //Añadir vertices (Init adjacency list)
+    adj = vector<list<uint>>(n);
+    for (uint i = 0; i < n; i++)
+        adj[i] = list<uint>(); //Inicializa lista vacía
+    
+    while (m--) { //Agregamos las aristas
+        uint u, v;
+        cin >> u >> v; // (u,v) = arista
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
 
     //TODO: Resolver el ejercicio T_T
