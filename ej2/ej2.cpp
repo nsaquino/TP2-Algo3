7b9b20c27192c07f405823c68f271f8b67805032
Nicolas Aquino <aquino_nicolas@hotmail.com>
2023-05-22 13:16:50 -0300

    Ej2 Bugeado

--- ej2/ej2.cpp
+++ ej2/ej2.cpp
@@ -5,58 +5,115 @@
 
 using namespace std;
 
-int n, m;       //n = cant. piezas
-vector<list<int>> D; //Lista de adyacencias
-vector<list<int>> D_T; //Grafo D con aristas invertidas
+int n, m;               //n = cant. piezas
+vector<list<int>> D;    //Lista de adyacencias
+vector<list<int>> D_T;  //Grafo D con aristas invertidas
+vector<int> cfc;        //Representantes de los cfc
+vector<int> cfc_D;      //Grafo de cfc
+int tam_cfc;
 
 enum {BLANCO, GRIS, NEGRO};
-vector<int> color;
+vector<bool> visitado;
 stack<int> S;
 
 void dfs1(int u){
-    color[u] = GRIS;
+    visitado[u] = true;
     for (list<int>::iterator it = D[u].begin(); it != D[u].end(); it++)
     {
         int v = *it;
-        if (color[v] == BLANCO)
+        if (!visitado[v])
             dfs1(v);
     }
-    color[u] = NEGRO;
     S.push(u);
 }
 
-void dfs2(int u){
-    color[u] = GRIS;
+void dfs2(int u, int representante){
+    visitado[u] = true;
     for (list<int>::iterator it = D_T[u].begin(); it != D_T[u].end(); it++)
     {
         int v = *it;
-        if (color[v] == BLANCO)
-            dfs2(v);
+        if (!visitado[v])
+            dfs2(v, representante);
     }
-    color[u] = NEGRO;
+    cfc[u] = representante;
 }
 
-int kosaraju(){
+void kosaraju(){
     //Paso 1
-    color = vector<int>(n, BLANCO);
+    visitado = vector<bool>(n, false);
     for (int i = 0; i < n; i++) {
-        if (color[i] == BLANCO) {
+        if (!visitado[i]) {
             dfs1(i);
         }
     }
     //Paso 2
     int cc = 0;
-    color = vector<int>(n, BLANCO);
+    cfc = vector<int>(n); //Vamos a decir a que cfc pertenece cada nodo
+    visitado = vector<bool>(n, false);
     while (!S.empty())
     {
         int u = S.top(); S.pop();
-        if (color[u] == BLANCO) {
-            dfs2(u);
+        if (!visitado[u]) {
+            cfc_D.push_back(u); //Para armar el digrafo nuevo
+            dfs2(u, u); //El nodo popeado (Que aun no se visito) es el representante
             cc++;
         }
     }
-    return cc;
+    tam_cfc = cc;
 }
+
+void dfs_topo(int u, vector<int> &top_sorted, int &orden){
+    visitado[u] = true;
+    S.push(u);
+    for (list<int>::iterator it = D[u].begin(); it != D[u].end(); it++)
+    {
+        int v = *it;        
+        if (!visitado[v] && cfc[v] != v)    //si no fue visit, y no toque su cfc, le aplico dfs_topo
+            dfs_topo(v, top_sorted, orden);
+    }
+    top_sorted[orden] = S.top();
+    S.pop();
+    orden--;
+}
+
+void topological_sort(){
+    //Stack esta vacio al iniciar
+    visitado = vector<bool>(n, false);
+    vector<int> top_sorted(tam_cfc);
+    int orden = tam_cfc - 1;
+    for (int i = 0; i < tam_cfc; i++) {
+        if (!visitado[cfc_D[i]]) {
+            dfs_topo(cfc_D[i], top_sorted, orden);
+        }
+    }
+    cfc_D = top_sorted; //cfc_D está ordenado topologicamente
+}
+
+
+
+vector<bool> visited;
+vector<int> res_temp;
+
+void dfs_sorted(int u){
+    visited[u] = true;
+    for (list<int>::iterator it = D[u].begin(); it != D[u].end(); it++)
+    {
+        int v = *it;        
+        if (!visitado[v])
+            dfs_sorted(v);
+    }
+}
+
+void last_dfs(){
+    visited = vector<bool>(n,false);
+    for(int i = 0; i < cfc_D.size(); i++){
+            if(visited[i]!=true){                
+                dfs_sorted(i);
+                res_temp.push_back(i);
+        }
+    }
+}
+
 
 int main(int argc, char *argv[]){
     cin >> n >> m;  //m = cant. pares de caída
@@ -88,6 +145,18 @@ int main(int argc, char *argv[]){
     }*/
 
     kosaraju();
+    topological_sort();
+
+    vector<int> res;
+    last_dfs();
+
+    //Imprimir tamaño de res_temp
+    cout << res_temp.size() << endl;
+    //Imprimir de a 1 los elementos de res_temp
+    for (int i = 0; i < res_temp.size(); i++){
+        cout << res_temp[i] + 1;
+    }
+    cout << endl;
 
     return 0;
 }
