34d4406dcf8509f9eafc7a56b752645c32145d7c
Nicolas Aquino <aquino_nicolas@hotmail.com>
2023-05-24 13:43:39 -0300

    Agrega funcion combinatoria

--- ej1/ej1.cpp
+++ ej1/ej1.cpp
@@ -2,11 +2,35 @@
 #include <vector>
 #include <list>
 
+using ll = long long;
 using namespace std;
 
 vector<list<uint>> adj; //Lista de adyacencias
 
+ll combinatoria_aux(vector<vector<ll>> &dp, ll n, ll k){
+    if (n < 0 || k < 0 || k > n) return 0;
+    if (k == 0 || k == n) return 1;
+
+    if(dp[n][k] != -1) return dp[n][k];
+    dp[n][k] = combinatoria_aux(dp, n-1, k-1) + combinatoria_aux(dp, n-1, k);
+    return dp[n][k];
+}
+
+ll combinatoria(ll n, ll k) {
+    if (n < 0 || k < 0 || k > n) return 0;
+    if (k == 0 || k == n) return 1;
+
+    vector<vector<ll>> dp(n+1 ,vector<ll>(k+1, -1));
+    return combinatoria_aux(dp, n, k);
+}
+
 int main(int argc, char *argv[]){
+
+    // //Debug combinatoria
+    // int N, K; cin >> N >> K;
+    // cout << combinatoria(N,K) << endl;
+    // return 0;
+
     int n, m;       //n = cant. vertices
     cin >> n >> m;  //m = cant. aristas
 
@@ -34,7 +58,6 @@ int main(int argc, char *argv[]){
         }
         cout << "}" << endl;
     }*/
-
     //TODO: Resolver el ejercicio T_T
 
     return 0;
