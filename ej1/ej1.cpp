2ee0691ced165c5bcb11d524e0529a887e304418
Nicolas Aquino <aquino_nicolas@hotmail.com>
2023-05-26 18:59:27 -0300

    Ej1 Terminado

--- ej1/ej1.cpp
+++ ej1/ej1.cpp
@@ -1,11 +1,109 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <bits/stdc++.h>    //sqrt(), pow()
 
 using ll = long long;
 using namespace std;
 
+enum state {NO_LO_VI, EMPECE_A_VER, TERMINE_DE_VER};
+
 int N, M;       //n = cant. vertices, m = cant. aristas
 vector<vector<int>> adj; //Lista de adyacencias
+vector<int> estado;
+
+//*************Encontrar puentes*************
+vector<vector<int>> treeEdges;
+vector<vector<int>> grafo_sin_puentes;
+//vector<vector<int>> bridges;
+vector<int> padreEnTree;
+vector<int> cant_vertices_en_cc;
+vector<ll> backConExtremoInferiorEn;
+vector<ll> backConExtremoSuperiorEn;
+vector<ll> memo_cubren; //init en -1, para la funcion 'cubren'
+
+
+
+void dfs_bridges(int v, int p) {
+    estado[v] = EMPECE_A_VER;
+    for (int u : adj[v]) {
+        if (estado[u] == NO_LO_VI) {
+            treeEdges[v].push_back(u);
+            padreEnTree[u] = v;
+            dfs_bridges(u, v);
+        } else if (u != p && estado[u] == EMPECE_A_VER) {
+            backConExtremoInferiorEn[v]++;
+            backConExtremoSuperiorEn[u]++;
+        }
+    }
+    estado[v] = TERMINE_DE_VER;
+}
+
+int cubren(int v) {
+    if (memo_cubren[v] != -1) return memo_cubren[v];
+
+    ll res = 0;
+    res += backConExtremoInferiorEn[v]; //backedge que cubren la arista p-v (con p = padre de v)
+    res -= backConExtremoSuperiorEn[v]; //Restamos los backedges de los decendientes de v que "terminan" en v
+    for (int hijo : treeEdges[v])       //Backedges de los descendientes de v 
+        res += cubren(hijo);
+
+    memo_cubren[v] = res;
+    return res;
+}
+
+int find_bridges(int r) { //r = root
+    estado = vector<int>(N, NO_LO_VI);
+    treeEdges = vector<vector<int>>(N, vector<int>());
+    backConExtremoInferiorEn = vector<ll>(N, 0);
+    backConExtremoSuperiorEn = vector<ll>(N, 0);
+    memo_cubren = vector<ll>(N, -1);
+    padreEnTree = vector<int>(N, -1);
+
+    grafo_sin_puentes = vector<vector<int>>(N, vector<int>());
+    
+    dfs_bridges(r, -1);
+    ll cant_puentes = 0;
+    for (int v = 0; v < N; v++){
+        if (cubren(v) == 0 && v != r){
+            cant_puentes++;
+            //int p = padreEnTree[v];
+            //bridges[min(p, v)].push_back(max(p, v));
+        } else if (v != r) {
+            int p = padreEnTree[v];
+            grafo_sin_puentes[p].push_back(v);
+            grafo_sin_puentes[v].push_back(p);
+        }
+    }
+    return cant_puentes;
+}
+//*************Fin encontrar puentes*************
+//*************Contar vertices en cc*************
+void dfs_count(int v, int cc) {
+    estado[v] = EMPECE_A_VER;
+    cant_vertices_en_cc[cc]++;
+    for (int u : grafo_sin_puentes[v]) {
+        if (estado[u] == NO_LO_VI) {
+            dfs_count(u, cc);
+        }
+    }
+    estado[v] = TERMINE_DE_VER;
+}
+
+void contar_vertices_en_cc(){
+    int cc = 0;
+    estado = vector<int>(N, NO_LO_VI);
+    
+    for (int i = 0; i < N; i++) {
+        if (estado[i] == NO_LO_VI) {
+            dfs_count(i, cc);
+            cc++;
+        }
+    }
+    //return cc;
+}
+//*************Fin contar vertices en cc*************
+
 
 ll combinatoria_aux(vector<vector<ll>> &dp, ll n, ll k){
     if (n < 0 || k < 0 || k > n) return 0;
@@ -41,7 +139,26 @@ int main(int argc, char *argv[]){
         adj[v-1].push_back(u-1);
     }
 
-    //TODO: Resolver el ejercicio T_T
+    int cant_puentes = find_bridges(0);
+    cant_vertices_en_cc = vector<int>(cant_puentes + 1, 0);
+    contar_vertices_en_cc();
+    ll cant_formas_de_jugar = combinatoria(N, 2);
+
+    //P(ganar) = (m/n) * ((m-1)/(n-1)) + ((n-m)/n) * ((n-m-1)/(n-1))    ???
+    //P(ganar) = ( comb(ver_cc,2)/ver_cc[i] ) * P(elegir cc)
+    //                                         1/cant_cc
+
+    ll cant_formas_de_ganar = 0;
+    ll ver_cc;
+    for(int i = 0; i < cant_vertices_en_cc.size(); i++){
+        ver_cc = cant_vertices_en_cc[i];
+        cant_formas_de_ganar += combinatoria(ver_cc, 2);
+    }
+
+    float res = 1.0 - ((1.0*cant_formas_de_ganar)/(1.0*cant_formas_de_jugar)); //((float)(res/(float) cant_formas_de_jugar));
+    
+    cout << fixed << setprecision(5); //fixed hace que aparezcan los .00000
+    cout << res << endl;    
 
     // //Debug
     // for (int i = 0; i < adj.size(); i++) {
@@ -53,3 +170,8 @@ int main(int argc, char *argv[]){
 
     return 0;
 }
+
+/*
+    1° calc puentes
+    2° para cada COMP hacer dfs y calcular cantidad, con eso calcular fórmula y listo.
+*/
